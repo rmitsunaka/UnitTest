2a91b645984d6d53d32389aece694a689fca03de
Guilherme Rangel <guilherme.rangel@sovos.com>
2021-11-30 10:21:14 -0300

    partiality commit

--- MyClassesTest/FileProcessTest.cs
+++ MyClassesTest/FileProcessTest.cs
@@ -1,4 +1,4 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;  //Restore nuget packages
 using MyClasses;
 using System;
 
@@ -7,11 +7,18 @@ namespace MyClassesTest
     [TestClass]
     public class FileProcessTest
     {
+        private const string BAD_FILE_NAME = @"C:\Windows\Xablau.exe";
+        private const string GOOD_FILE_NAME = @"C:\Windows\Regedit.exe";
+
+        public TestContext TestContext { get; set; }
+
         [TestMethod]
         public void FileNameDoesExists()
         {
             FileProcess fp = new FileProcess();
             bool fromCall;
+
+            TestContext.WriteLine(@"Checking file " + GOOD_FILE_NAME);
 
             fromCall = fp.FileExists(@"C:\Windows\Regedit.exe");
 
@@ -23,6 +30,8 @@ namespace MyClassesTest
         {
             FileProcess fp = new FileProcess();
             bool fromCall;
+
+            TestContext.WriteLine(@"Checking file " + BAD_FILE_NAME);
 
             fromCall = fp.FileExists(@"C:\Windows\Xablau.exe");
 
